a8931b893426aa3186929f80532dc35682e963aa
Teperi Lee <teperi31@gmail.com>
2018-12-31 15:55:42 +0900

    회원가입 중복가입 방지 코드 제작 완료 회원가입 및 로그인 기능 구현 완료

--- php/connectDB.php
+++ php/connectDB.php
@@ -1,12 +1,13 @@
-<!-- PHP 와 MYSQL 연결해주는 PHP
+
+
+<?php
+/* PHP 와 MYSQL 연결해주는 PHP
 	require_once 'connectDB.php';
 	을 다른 파일어 넣은 후
 	$conn 으로 사용 가능함
 
 	서버가 죽었을 경우 error 띄움
--->
-
-<?php
+*/
 $host='localhost';
 $user='root';
 $pw='hievening5';
---
+++ php/signupcheck.php
@@ -0,0 +1,38 @@
+<?php
+require_once 'connectDB.php';
+
+if( isset($_POST['id']) ){
+    $uid = $_POST["id"];
+    $sql = "SELECT * FROM user_info WHERE id = '$uid'";
+    // DB 에 쿼리 날리기
+    $result = mysqli_query($conn,$sql);
+        
+    // 쿼리 결과를 PHP 에서 사용할 수 있도록 변경
+    $row = mysqli_fetch_assoc($result);
+
+    echo $row['id'];
+
+} else if (isset($_POST['nick'])){
+    $nick = $_POST["nick"];
+    $sql = "SELECT * FROM user_info WHERE nickName = '$nick'";
+    // DB 에 쿼리 날리기
+    $result = mysqli_query($conn,$sql);
+        
+    // 쿼리 결과를 PHP 에서 사용할 수 있도록 변경
+    $row = mysqli_fetch_assoc($result);
+
+    echo $row['id'];
+} else if (isset($_POST['email'])) {
+    $email = $_POST["email"];
+    $sql = "SELECT * FROM user_info WHERE email = '$email'";
+    // DB 에 쿼리 날리기
+    $result = mysqli_query($conn,$sql);
+        
+    // 쿼리 결과를 PHP 에서 사용할 수 있도록 변경
+    $row = mysqli_fetch_assoc($result);
+
+    echo $row['id'];
+}
+
+
+?>
