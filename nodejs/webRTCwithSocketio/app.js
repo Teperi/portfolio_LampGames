2f770e02ce76262419ead18c658004eb6b1c94c9
Teperi Lee <teperi31@gmail.com>
2019-01-17 22:43:16 +0900

    webRTC 이미지 전송 테스트

--- nodejs/webRTCwithSocketio/app.js
+++ nodejs/webRTCwithSocketio/app.js
@@ -1,15 +1,24 @@
-var express = require("express");
-var app = new express();
-var http = require("http").Server(app);
-var io = require("socket.io")(http);
+var express = require('express'); // Express contains some boilerplate to for routing and such
+var app = express();
+// http 통신
+var http = require('http').Server(app);
+// socket.io 추가
+var io = require('socket.io')(http);
 
-var Log = require('log'),
-    log = new Log('debug')
+app.use(express.static('public'));
+// We define a route handler '/' that gets called when we hit our website home.
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + 'index.html');
+});
 
-var port = process.env.PORT || 3000;
+io.on('connection', (socket) => {
+    socket.on('stream', (video) => {
+        socket.broadcast.emit('stream', video);
+    });
+});
 
-app.use(express.static(__dirname + "/public"));
 
-app.listen(port, () => {
-    log.info('Listen port %s', port);
+//We make the http server listen on port 3000.
+http.listen(8080, function() {
+    console.log('listening on *:8080');
 });
